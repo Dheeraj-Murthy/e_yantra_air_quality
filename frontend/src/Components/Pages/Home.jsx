1884ce6afc9c536be29e8ad52f41433bf4411132
meenigam <ms.dheerajmurthy@iiitb.ac.in>
2024-11-02 02:36:13 +0530

    just redid some frontend stuff cause the framework files were corrupted

---
+++ frontend/src/Components/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="h-screen w-screen bg-blue-100">
+      <div className='text-xl font-bold underline'>Hello wrold</div>
+    </div >
+  )
+}
+
+export default Home
